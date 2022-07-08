63437e27254b47c6cd1c35037d1dfe0e766346ef
Dwaipayan <dwaipayan.sen@cimpress.com>
2022-07-08 17:56:27 +0530

    Initail commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const setShowGroomPage = () => {
+    setIdVisible("groomPage")
+    setIdHidden("homePage")
+}
+
+const setShowHomePage = () => {
+    setIdVisible("homePage")
+    setIdHidden("groomPage")
+}
+
+const groomButton = document.getElementById("groomButton")
+groomButton.addEventListener('click', setShowGroomPage)
+
+const homeButton = document.getElementById("homeButton")
+homeButton.addEventListener('click', setShowHomePage)
+
+function setIdHidden(id) {
+    const element = document.getElementById(id)
+    // element.classList.remove("visible")
+    element.classList.add("hidden")
+}
+
+function setIdVisible(id) {
+    const element = document.getElementById(id)
+    element.classList.remove("hidden")
+    // element.classList.add("visible")
+}
