df1b3ef18958de8fe4b4f9b2333aa0a8ee74f68f
Dwaipayan <dwaipayan.sen@cimpress.com>
2022-07-08 19:37:48 +0530

    Add bangla font

--- index.js
+++ index.js
@@ -25,3 +25,9 @@ function setIdVisible(id) {
     element.classList.remove("hidden")
     // element.classList.add("visible")
 }
+
+const enterText = "প্রবেশ"
+// const enterText = "enter"
+const arrows = document.querySelectorAll(".arrow")
+arrows.forEach(arrow => arrow.textContent = enterText)
+// arrow.textContent = enterText
